fb312356974cbc5e48354133ae5d65914f7a5f6d
Your Name <hans@hanken.org>
2017-10-10 18:44:07 +0000

    Lesson 14 - Handle form submission DONE

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,4 +9,12 @@ def new
   @place = Place.new
 end
 
+def create
+Place.create(place_params)
+redirect_to root_path
+end
+
+def place_params
+  params.require(:place).permit(:name, :description, :address)
+end
 end
