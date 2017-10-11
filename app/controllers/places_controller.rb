eabc28b58717572ba94452529cb7403919380170
Your Name <hans@hanken.org>
2017-10-11 20:37:11 +0000

    Lesson 18 - Connect user to Places, DONE!

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
 def index
   @places = Place.order(:name).page(params[:page])
@@ -10,7 +11,7 @@ def new
 end
 
 def create
-Place.create(place_params)
+current_user.places.create(place_params)
 redirect_to root_path
 end
 
