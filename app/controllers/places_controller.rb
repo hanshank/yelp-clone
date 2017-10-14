311bdd9b8b98a23112f3c55590a1ac97c71ffac0
Your Name <hans@hanken.org>
2017-10-14 16:41:36 +0000

    Lesson 26, Destroy Permissions DONE!

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 def index
   @places = Place.order(:name).page(params[:page])
@@ -23,7 +23,7 @@ def edit
 @place = Place.find(params[:id])
 if @place.user != current_user
   return render text: 'Not Allowed', status: :forbidden
-end 
+end
 
 if @place.user != current_user
   return render text: 'Not Allowed', status: :forbidden
@@ -37,6 +37,9 @@ end
 
 def destroy
 @place = Place.find(params[:id])
+if @place.user != current_user
+  return render text: 'Not Allowed, status: :forbidden'
+end
 @place.destroy
 redirect_to root_path
 end
