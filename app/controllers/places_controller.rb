ad161d254ce66121246e50b7681b8c1e15a02f91
Your Name <hans@hanken.org>
2017-10-14 16:35:09 +0000

    Lesson 25, Edit User Permissions, DONE!

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
 def index
   @places = Place.order(:name).page(params[:page])
@@ -21,6 +21,12 @@ end
 
 def edit
 @place = Place.find(params[:id])
+if @place.user != current_user
+  return render text: 'Not Allowed', status: :forbidden
+end 
+
+if @place.user != current_user
+  return render text: 'Not Allowed', status: :forbidden
 end
 
 def update
