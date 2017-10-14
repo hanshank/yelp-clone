fc0b8307e8526835eb06be81f6cec7217efc2e38
Your Name <hans@hanken.org>
2017-10-14 14:08:23 +0000

    Lesson 23 DONE

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -23,6 +23,12 @@ def edit
 @place = Place.find(params[:id])
 end
 
+def update
+@place = Place.find(params[:id])
+@place.update_attributes(place_params)
+redirect_to root_path
+end
+
 private
 
 def place_params
