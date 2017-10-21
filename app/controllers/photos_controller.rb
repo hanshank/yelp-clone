99bee3954b99af03c4cd266664476b980993eb10
Your Name <hans@hanken.org>
2017-10-21 17:11:08 +0000

    Photo function continued

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,16 @@
-class PhotosController < ApplicationRecord
-  before_action :authenticate_user!
+class Photos_controller < ApplicationRecord
+
+  def new
+    @photo = Photo.new
+  end
 
   def create
-    @place = Place.find(params[:place_id])
-    @place.comments.create(comment_params.merge(user: current_user))
-    redirect_to place_path(@place)
+    @photo = current_user.photos.create(place_params)
+    if @photo.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,2 @@
 class Photo < ApplicationRecord
-  belongs_to :user
-  has_many :photos
-
-
-
 end
