96971ac7132c55996e7948f93e6b6436b8028bbc
Your Name <hans@hanken.org>
2017-10-22 18:54:08 +0000

    Lesson 40 DONE

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationMailer < ApplicationMailer
+  default from: "no-reply@nomsterapp.com"
+
+
+  def comment_added
+    mail(to: "hans@hanken.org",
+        subject: "A comment has been added to your place")
+  end
+end
