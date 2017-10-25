33e34f8c265dcc1038f1d9095ef0146693915a48
Your Name <hans@hanken.org>
2017-10-25 18:25:22 +0000

    Buttons fixed and lesson 42 DONE

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,6 +6,6 @@ class NotificationMailer < ApplicationMailer
     @place = comment.place
     @place_owner = @place.user
     mail(to: @place_owner.email,
-        subject: "A comment has been added to your place")
+        subject: "A comment has been added to #{@place.name}")
   end
 end
